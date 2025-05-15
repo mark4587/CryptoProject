5caa7d2110a8e480859d0347aad262ecb1c0756f
Mark Doyle <76437684+mark4587@users.noreply.github.com>
2025-05-15 22:22:34 +0100

    Final Version

--- api/handlers/base.py
+++ api/handlers/base.py
@@ -1,3 +1,5 @@
+# Mark Doyle
+# C00257481
 from json import dumps, loads
 from tornado.web import RequestHandler
 
@@ -5,6 +7,7 @@ class BaseHandler(RequestHandler):
 
     @property
     def db(self):
+        # Gives access to the MongoDB database connection
         return self.application.db
 
     @property
@@ -14,6 +17,7 @@ class BaseHandler(RequestHandler):
     def prepare(self):
         if self.request.body:
             try:
+                # Reads JSON data
                 json_data = loads(self.request.body)
                 self.request.arguments.update(json_data)
             except ValueError:
@@ -26,13 +30,17 @@ class BaseHandler(RequestHandler):
         self.set_header('Access-Control-Allow-Methods', '*')
         self.set_header('Access-Control-Allow-Headers', '*')
 
+
+    # Prompts Error Messages Where Needed
     def write_error(self, status_code, **kwargs):
         if 'message' not in kwargs:
             if status_code == 405:
                 kwargs['message'] = 'Invalid HTTP method.'
             else:
                 kwargs['message'] = 'Unknown error.'
-        self.response = kwargs
+        self.response = {
+            "error": kwargs['message']
+        }
         self.write_json()
 
     def write_json(self):
@@ -43,3 +51,5 @@ class BaseHandler(RequestHandler):
         self.set_status(204)
         self.finish()
 
+
+ 
