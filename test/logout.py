efc2c94005e6b73bba1071fcc67e6e7fb9935419
Mark Doyle <76437684+mark4587@users.noreply.github.com>
2025-05-16 12:37:56 +0100

    Final Version

--- test/logout.py
+++ test/logout.py
@@ -4,11 +4,8 @@ from tornado.gen import coroutine
 from tornado.httputil import HTTPHeaders
 from tornado.ioloop import IOLoop
 from tornado.web import Application
-
 from api.handlers.logout import LogoutHandler
-
 from .base import BaseTest
-
 import urllib.parse
 
 class LogoutHandlerTest(BaseTest):
