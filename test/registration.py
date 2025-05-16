d16303ae11a3b3792e8a6d755f8724cb0ddf3bf6
Mark Doyle <76437684+mark4587@users.noreply.github.com>
2025-05-16 12:38:54 +0100

    Final Version

--- test/registration.py
+++ test/registration.py
@@ -1,62 +1,49 @@
 from json import dumps
 from tornado.escape import json_decode
-from tornado.ioloop import IOLoop
 from tornado.web import Application
-
 from api.handlers.registration import RegistrationHandler
-
 from .base import BaseTest
-
-import urllib.parse
 
 class RegistrationHandlerTest(BaseTest):
 
     @classmethod
-    def setUpClass(self):
-        self.my_app = Application([(r'/registration', RegistrationHandler)])
+    def setUpClass(cls):
+        # Calls handlers 
+        cls.my_app = Application(
+        [
+            (r'/registration', RegistrationHandler),
+        ])
         super().setUpClass()
 
+  # User Information
     def test_registration(self):
         email = 'test@test.com'
-        display_name = 'testDisplayName'
+        display_name = 'testName'
+        address = '123 White Street'
+        dob = '01-01-2001'
+        phone_number = '123456789'
+        disabilities = 'asthma'
 
+      # Creates the user with the pulled information 
         body = {
-          'email': email,
-          'password': 'testPassword',
-          'displayName': display_name
+            'email': email,
+            'password': 'testPassword123',
+            'displayName': display_name,
+            'address': address,
+            'dob': dob,
+            'phoneNumber': phone_number,
+            'disabilities': disabilities
         }
 
         response = self.fetch('/registration', method='POST', body=dumps(body))
+        # Checks server connection with 200 code
         self.assertEqual(200, response.code)
 
-        body_2 = json_decode(response.body)
-        self.assertEqual(email, body_2['email'])
-        self.assertEqual(display_name, body_2['displayName'])
-
-    def test_registration_without_display_name(self):
-        email = 'test@test.com'
-
-        body = {
-          'email': email,
-          'password': 'testPassword'
-        }
-
-        response = self.fetch('/registration', method='POST', body=dumps(body))
-        self.assertEqual(200, response.code)
-
-        body_2 = json_decode(response.body)
-        self.assertEqual(email, body_2['email'])
-        self.assertEqual(email, body_2['displayName'])
-
-    def test_registration_twice(self):
-        body = {
-          'email': 'test@test.com',
-          'password': 'testPassword',
-          'displayName': 'testDisplayName'
-        }
-
-        response = self.fetch('/registration', method='POST', body=dumps(body))
-        self.assertEqual(200, response.code)
-
-        response_2 = self.fetch('/registration', method='POST', body=dumps(body))
-        self.assertEqual(409, response_2.code)
+        # Decodes the JSON information and is placed into 'data'
+        data = json_decode(response.body)
+        self.assertEqual(email, data['email'])
+        self.assertEqual(display_name, data['displayName'])
+        self.assertEqual(address, data['address'])
+        self.assertEqual(dob, data['dob'])
+        self.assertEqual(phone_number, data['phoneNumber'])
+        self.assertEqual(disabilities, data['disabilities'])
