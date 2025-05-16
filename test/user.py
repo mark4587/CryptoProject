12dda6704cc1ee5c293fcbf10d02c2380ff629a9
Mark Doyle <76437684+mark4587@users.noreply.github.com>
2025-05-16 12:40:39 +0100

    Final Version

--- test/user.py
+++ test/user.py
@@ -1,66 +1,52 @@
-from json import dumps
-from tornado.escape import json_decode, utf8
-from tornado.gen import coroutine
-from tornado.httputil import HTTPHeaders
-from tornado.ioloop import IOLoop
+import json
 from tornado.web import Application
-
+from api.handlers.registration import RegistrationHandler
+from api.handlers.login import LoginHandler
 from api.handlers.user import UserHandler
-
 from .base import BaseTest
-
-import urllib.parse
 
 class UserHandlerTest(BaseTest):
 
     @classmethod
-    def setUpClass(self):
-        self.my_app = Application([(r'/user', UserHandler)])
+    def setUpClass(cls):
+        # Calls handlers
+        cls.my_app = Application([
+            (r'/registration', RegistrationHandler),
+            (r'/login', LoginHandler),
+            (r'/user', UserHandler),
+        ])
         super().setUpClass()
-
-    @coroutine
-    def register(self):
-        yield self.get_app().db.users.insert_one({
-            'email': self.email,
-            'password': self.password,
-            'displayName': self.display_name
-        })
-
-    @coroutine
-    def login(self):
-        yield self.get_app().db.users.update_one({
-            'email': self.email
-        }, {
-            '$set': { 'token': self.token, 'expiresIn': 2147483647 }
-        })
 
     def setUp(self):
         super().setUp()
+        # Registers the test user
+        body = {
+            "email": "test@test.com",
+            "password": "testPassword123",
+            "displayName": "testName",
+            "address": "123 White Street",
+            "dob": "01-01-2001",
+            "phoneNumber": "123456789",
+            "disabilities": "asthma"
+        }
+        self.fetch('/registration', method='POST', body=json.dumps(body))
 
-        self.email = 'test@test.com'
-        self.password = 'testPassword'
-        self.display_name = 'testDisplayName'
-        self.token = 'testToken'
-
-        IOLoop.current().run_sync(self.register)
-        IOLoop.current().run_sync(self.login)
+        # Logs in to get token
+        login_body = {
+            "email": "test@test.com",
+            "password": "testPassword123"
+        }
+        response = self.fetch('/login', method='POST', body=json.dumps(login_body))
+        self.token = json.loads(response.body.decode())['token']
 
     def test_user(self):
-        headers = HTTPHeaders({'X-Token': self.token})
-
-        response = self.fetch('/user', headers=headers)
+        response = self.fetch('/user', method='GET', headers={"X-Token": self.token})
         self.assertEqual(200, response.code)
 
-        body_2 = json_decode(response.body)
-        self.assertEqual(self.email, body_2['email'])
-        self.assertEqual(self.display_name, body_2['displayName'])
-
-    def test_user_without_token(self):
-        response = self.fetch('/user')
-        self.assertEqual(400, response.code)
-
-    def test_user_wrong_token(self):
-        headers = HTTPHeaders({'X-Token': 'wrongToken'})
-
-        response = self.fetch('/user')
-        self.assertEqual(400, response.code)
+        body = json.loads(response.body)
+        self.assertEqual("test@test.com", body["email"])
+        self.assertEqual("testName", body["displayName"])
+        self.assertEqual("123 White Street", body["address"])
+        self.assertEqual("01-01-2001", body["dob"])
+        self.assertEqual("123456789", body["phoneNumber"])
+        self.assertEqual("asthma", body["disabilities"])
